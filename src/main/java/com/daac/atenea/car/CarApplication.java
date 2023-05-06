6fd0c887aa8f58ab7f2318726abcd1a2cee0326f
David811 <54964434+DAAC11@users.noreply.github.com>
2023-05-05 21:32:45 -0500

    Update CarApplication.java

--- src/main/java/com/daac/atenea/car/CarApplication.java
+++ src/main/java/com/daac/atenea/car/CarApplication.java
@@ -2,9 +2,10 @@ package com.daac.atenea.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class
 CarApplication {
 
